db91f22a818f9d3cff66230304825bcaf6598da9
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-02-16 12:01:49 +0800

    Update LXGW WenKai fonts to version 1.200
    Now package files which contain all the TTF fonts are provided
    officially [1].
    
    [1]: https://github.com/lxgw/LxgwWenKai/releases/tag/v1.200

--- Casks/ttf-lxgw-wenkai-mono.rb
+++ Casks/ttf-lxgw-wenkai-mono.rb
@@ -1,21 +1,21 @@
 cask "ttf-lxgw-wenkai-mono" do
-  version "1.120"
-  sha256 "f1389ad24a6c8f882fcfa8428034754dae3204ebb09bfa04d758a5d9b946849f"
+  version "1.200"
+  sha256 "ab8428c4fe95e6a08721e1566e941042104559bce28195d3ae8dc279f6c25c45"
 
-  url "https://github.com/oh-my-home/lxgw-wenkai/releases/download/v#{version}/lxgw-wenkai-mono-#{version}.zip",
-    verified: "github.com/oh-my-home/lxgw-wenkai/"
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip",
+    verified: "github.com/lxgw/LxgwWenKai/"
   name "LXGW WenKai Mono"
   name "霞鹜文楷等宽"
   homepage "https://github.com/lxgw/LxgwWenKai"
 
   livecheck do
-    url "https://github.com/oh-my-home/lxgw-wenkai"
+    url "https://github.com/lxgw/LxgwWenKai"
     strategy :github_latest
   end
 
-  font "TTF/LXGWWenKaiMono-Bold.ttf"
-  font "TTF/LXGWWenKaiMono-Light.ttf"
-  font "TTF/LXGWWenKaiMono-Regular.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Bold.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Light.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Regular.ttf"
 
   caveats do
     free_license "https://github.com/lxgw/LxgwWenKai/raw/v#{version}/SIL_Open_Font_License_1.1.txt"
--- Casks/ttf-lxgw-wenkai.rb
+++ Casks/ttf-lxgw-wenkai.rb
@@ -1,21 +1,21 @@
 cask "ttf-lxgw-wenkai" do
-  version "1.120"
-  sha256 "2801575b7d9e5b2efaf5e571e273845a1e9c0e00381247bd4a3f8aa1566c6196"
+  version "1.200"
+  sha256 "ab8428c4fe95e6a08721e1566e941042104559bce28195d3ae8dc279f6c25c45"
 
-  url "https://github.com/oh-my-home/lxgw-wenkai/releases/download/v#{version}/lxgw-wenkai-#{version}.zip",
-    verified: "github.com/oh-my-home/lxgw-wenkai/"
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip",
+    verified: "github.com/lxgw/LxgwWenKai/"
   name "LXGW WenKai"
   name "霞鹜文楷"
   homepage "https://github.com/lxgw/LxgwWenKai"
 
   livecheck do
-    url "https://github.com/oh-my-home/lxgw-wenkai"
+    url "https://github.com/lxgw/LxgwWenKai"
     strategy :github_latest
   end
 
-  font "TTF/LXGWWenKai-Bold.ttf"
-  font "TTF/LXGWWenKai-Light.ttf"
-  font "TTF/LXGWWenKai-Regular.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Bold.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Light.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Regular.ttf"
 
   caveats do
     free_license "https://github.com/lxgw/LxgwWenKai/raw/v#{version}/SIL_Open_Font_License_1.1.txt"
