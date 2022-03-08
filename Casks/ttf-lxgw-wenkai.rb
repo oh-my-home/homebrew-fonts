0b6675aba94d38747ba1619563a1526b2abfc617
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-03-08 15:00:03 +0800

    Update LXGW WenKai fonts to version 1.222

--- Casks/ttf-lxgw-wenkai.rb
+++ Casks/ttf-lxgw-wenkai.rb
@@ -1,6 +1,6 @@
 cask "ttf-lxgw-wenkai" do
-  version "1.220"
-  sha256 "38efb1480f921f09bb54a60ae70d57d1baad019867cf7483b85a8245455952cf"
+  version "1.222"
+  sha256 "b593bef64fe20baafa0111a462288c2fd4c7d2d696583990a306b2d6493f60a8"
 
   url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip",
     verified: "github.com/lxgw/LxgwWenKai/"
