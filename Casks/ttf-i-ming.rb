5c843e05e26044f288bb43d65f027afab1829221
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-09-12 21:39:52 +0800

    Update I.Ming fonts to version 8.00

--- Casks/ttf-i-ming.rb
+++ Casks/ttf-i-ming.rb
@@ -1,10 +1,12 @@
 cask "ttf-i-ming" do
-  version "7.01"
-  sha256 "6380f19ca5a7afa7b9802c75d9bef767b6802c24177adbd3eb171e4fc44c9913"
+  version "8.00"
+  sha256 "b71321a6da01f48341c89be23bc0bf19565c94de4b154ced841b377241e2047d"
 
   url "https://github.com/oh-my-home/i.ming/releases/download/#{version}/i.ming-#{version}.zip",
     verified: "github.com/oh-my-home/i.ming/"
-  name "I.明體（I.Ming）"
+  name "I.Ming"
+  name "一点明体"
+  name "一點明體"
   homepage "https://github.com/ichitenfont/I.Ming"
 
   livecheck do
@@ -12,10 +14,12 @@ cask "ttf-i-ming" do
     strategy :github_latest
   end
 
-  font "I.Ming-#{version}.ttf"
-  font "I.MingCP-#{version}.ttf"
-  font "I.MingVar-#{version}.ttf"
-  font "I.MingVarCP-#{version}.ttf"
+  font "i.ming-#{version}/I.Ming-#{version}.ttf"
+  font "i.ming-#{version}/I.MingCP-#{version}.ttf"
+  font "i.ming-#{version}/I.MingVar-#{version}.ttf"
+  font "i.ming-#{version}/I.MingVarCP-#{version}.ttf"
+  font "i.ming-#{version}/PMingI.U-#{version}.ttf"
+  font "i.ming-#{version}/PMingI.UVar-#{version}.ttf"
 
   caveats do
     free_license "https://github.com/ichitenfont/I.Ming/raw/#{version}/#{version}/IPA_Font_License_Agreement_v1.0.md"
