416b359b7ad8d0c5a9623a255a10392d91ffb207
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-10-05 13:16:21 +0800

    Add Fragment Mono fonts

---
+++ Casks/ttf-fragment-mono.rb
@@ -0,0 +1,21 @@
+cask "ttf-fragment-mono" do
+  version "1.003"
+  sha256 "ed7388863c36c33d33e801d090ecf865cbb7f5d98719f9ab735a21c116691710"
+
+  url "https://github.com/weiweihuanghuang/fragment-mono/releases/download/#{version}/fragment-mono-fonts.zip",
+    verified: "github.com/weiweihuanghuang/fragment-mono/"
+  name "Fragement Mono"
+  homepage "https://github.com/weiweihuanghuang/fragment-mono"
+
+  livecheck do
+    url "https://github.com/weiweihuanghuang/fragment-mono"
+    strategy :github_latest
+  end
+
+  font "fragment-mono-fonts/fonts/ttf/FragmentMono-Italic.ttf"
+  font "fragment-mono-fonts/fonts/ttf/FragmentMono-Regular.ttf"
+
+  caveats do
+    free_license "https://github.com/weiweihuanghuang/fragment-mono/raw/#{version}/OFL.txt"
+  end
+end
