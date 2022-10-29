29b4015f74859b7f4be434b363318504b391bf94
Kaizhao Zhang <zhangkaizhao@gmail.com>
2022-10-29 18:37:33 +0800

    Update Twitter Color Emoji font to version 14.0.2

--- Casks/ttf-twitter-color-emoji.rb
+++ Casks/ttf-twitter-color-emoji.rb
@@ -1,20 +1,20 @@
 cask "ttf-twitter-color-emoji" do
-  version "13.1.0"
-  sha256 "4ef8af7ebe5f04ed40bc76ff6bc02ed8ebeb252af1468462eba80ca3b8682e97"
+  version "14.0.2"
+  sha256 "8e2c6cb768b5a578b1dacb8f70d3b91e782a8098821484af77cf322ac499f28a"
 
-  url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip",
-    verified: "github.com/eosrei/twemoji-color-font/"
+  url "https://github.com/13rac1/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip",
+    verified: "github.com/13rac1/twemoji-color-font/"
   name "Twitter Color Emoji"
-  homepage "https://github.com/eosrei/twemoji-color-font"
+  homepage "https://github.com/13rac1/twemoji-color-font"
 
   livecheck do
-    url "https://github.com/eosrei/twemoji-color-font"
+    url "https://github.com/13rac1/twemoji-color-font"
     strategy :github_latest
   end
 
   font "TwitterColorEmoji-SVGinOT-#{version}/TwitterColorEmoji-SVGinOT.ttf"
 
   caveats do
-    license "https://raw.githubusercontent.com/eosrei/twemoji-color-font/v#{version}/LICENSE.md"
+    license "https://raw.githubusercontent.com/13rac1/twemoji-color-font/v#{version}/LICENSE.md"
   end
 end
