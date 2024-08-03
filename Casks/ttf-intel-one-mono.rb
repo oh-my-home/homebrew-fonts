522579279f24e98b3e1dae8b002dd352999ccdc2
Kaizhao Zhang <zhangkaizhao@gmail.com>
2024-08-03 16:15:48 +0800

    Update Intel One Mono fonts to version 1.4.0

--- Casks/ttf-intel-one-mono.rb
+++ Casks/ttf-intel-one-mono.rb
@@ -1,6 +1,6 @@
 cask "ttf-intel-one-mono" do
-  version "1.3.0"
-  sha256 "11e51310cba84c72a6b8ee548f48e3883445f6dee3c5b232e399d35a8ce581f7"
+  version "1.4.0"
+  sha256 "54863552d25dcb9c3f5360b296fc980d6e1fbfd02e0d214224e8b78f0a2bccf0"
 
   url "https://github.com/intel/intel-one-mono/releases/download/V#{version}/ttf.zip",
     verified: "github.com/intel/intel-one-mono/"
